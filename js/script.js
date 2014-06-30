f095e8fc9bce3b1d6577a36c25fa5c53d4bb4f8c
Jonathan Webb <jwebbed@bell.net>
2014-06-30 14:55:30 -0400

    IT WORKS GG NO RE

--- js/script.js
+++ js/script.js
@@ -5,6 +5,44 @@ var courses = {};
 var depth = {};
 var children = {};
 
+var buttons = [];
+
+function tableRowCallback(i, row){
+ 	var jrow = $(row);
+ 	var course = $(jrow.find("td")[0]).text();
+ 	var course_sem = $(jrow.find("td")[1]).text();
+ 	var course_code = course + course_sem;
+
+ 	if (!(i <= 2) && course !== String.fromCharCode(160)){
+ 		if (depth[prev_course_code] === 0){
+ 			prev_row.find("button").remove();
+ 		} else if (prev_row !== null) {
+ 			prev_row.find("button").click();
+ 		}
+ 		if (i !== size){
+ 			jrow.prepend("<td align='LEFT'><font size='-1'><button onClick='minMax(this)'>-</button></font></td>");
+
+
+ 			jrow.attr("id", course_code);
+ 			prev_row = jrow;
+ 			prev_course_code = course_code;
+ 			courses[course_code] = 1;
+ 			depth[course_code] = 0;
+ 			children[course_code] = [];
+ 		} else {
+ 			jrow.prepend('<td align="LEFT"><font size="-1">&nbsp;</font></td>');
+ 		}
+ 	} else {
+ 		jrow.prepend('<td align="LEFT"><font size="-1">&nbsp;</font></td>');
+ 		if (prev_course_code !== null){
+ 			jrow.addClass(prev_course_code);
+ 			depth[prev_course_code] += 1;
+ 			children[prev_course_code].push(jrow);
+ 		}
+ 	}
+}
+
+
 function minMax(obj){
 	var row = obj.parentElement.parentElement.parentElement;
 
@@ -26,51 +64,13 @@ function minMax(obj){
 		try {
 			$("#" + row.id).find("td")[10].rowSpan += depth[row.id];
 		} catch (TypeError) {
-			console.log(row.id.slice(0, 8));
+			//console.log(row.id.slice(0, 8));
 			$("#" + row.id.slice(0, 8) + "F").find("td")[10].rowSpan += depth[row.id];
 		}
 		$("." + row.id).show();
 		obj.innerHTML = "-";
 		courses[row.id] = 1;
 	}
-}
-
-function tableRowCallback(i, row){
- 	var jrow = $(row);
- 	var course = $(jrow.find("td")[0]).text();
- 	var course_sem = $(jrow.find("td")[1]).text();
- 	var course_code = course + course_sem;
-
- 	if (!(i <= 2) && course !== String.fromCharCode(160)){
- 		if (depth[prev_course_code] === 0){
- 			prev_row.find("button").remove();
- 		} else if (prev_row !== null) {
- 			prev_row.find("button").click();
- 		}
- 		if (i !== size){
- 			var thing = Math.random();
- 			jrow.prepend("<td align='LEFT'><font size='-1'><button id = '" + thing + "'>-</button></font></td>");
- 			$('#' + thing).click(function () {
- 				minMax(this);
- 			});
-
- 			jrow.attr("id", course_code);
- 			prev_row = jrow;
- 			prev_course_code = course_code;
- 			courses[course_code] = 1;
- 			depth[course_code] = 0;
- 			children[course_code] = [];
- 		} else {
- 			jrow.prepend('<td align="LEFT"><font size="-1">&nbsp;</font></td>');
- 		}
- 	} else {
- 		jrow.prepend('<td align="LEFT"><font size="-1">&nbsp;</font></td>');
- 		if (prev_course_code !== null){
- 			jrow.addClass(prev_course_code);
- 			depth[prev_course_code] += 1;
- 			children[prev_course_code].push(jrow);
- 		}
- 	}
 }
 
 function init () {
@@ -82,6 +82,12 @@ function init () {
 	rows.each(tableRowCallback);
 }
 
+window.onload = init
+
+/*
 $(document).ready(function() {
 	init();
 });
+*/
+
+
