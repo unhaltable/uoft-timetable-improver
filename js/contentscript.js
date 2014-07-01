a1da515786a258d580912063cd6ee641d0bb429f
Spencer Elliott <me@elliottsj.com>
2014-06-30 21:00:04 -0400

    Only add buttons to courses with > 1 section

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,21 +1,27 @@
 // Add a class to the table for styling
-document.getElementsByTagName('table')[0].id = 'courses';
+var $table = $('table:first').attr('id', 'courses');
 
 // Keep a reference to the set of all rows
-var $allRows = $('#courses tr');
+var $allRows = $table.find('tr');
 
-// For each course row... (rows that don't contain a &nbsp; in the first column)
+// Course rows (rows that contain a course code)
 var $courseRows = $allRows.slice(3).has('font:first:not(:contains("\u00a0"))');
 
 // Add a class to the course rows
 $courseRows.addClass('course-row');
 
+// Find courses with multiple sections
+var $courseRowsCollapsable = $courseRows.filter(function () {
+    // Row is collapsable if the next row has a &nbsp; in the first column
+    return $(this).next().has('font:first:contains("\u00a0")').length > 0
+});
+
 // Add a button before each course row to expand/collapse the section rows
 $('<td align="LEFT"><font size="-1"><button>-</button></font></td>')
-  .prependTo($courseRows)
+  .prependTo($courseRowsCollapsable)
   .find('button').click(function() {
     var $button = $(this);
-    var $row = $button.parents('tr');
+    var $row = $button.closest('tr');
 
     // Toggle visibility of course section rows
     $row.nextUntil('tr.course-row').toggle();
@@ -27,4 +33,4 @@ $('<td align="LEFT"><font size="-1"><button>-</button></font></td>')
   });
 
 // Add a blank cell before each rows that doesn't have a button
-$allRows.not($courseRows).prepend('<td align="LEFT"><font size="-1">&nbsp;</font></td>');
+$allRows.not($courseRowsCollapsable).prepend('<td align="LEFT"><font size="-1">&nbsp;</font></td>');
